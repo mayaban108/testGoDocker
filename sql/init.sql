35d9a8a6324fe5944e5622dc629cefa983fdc963
mayaban108 <maya.bannis@wepala.com>
2020-09-03 13:10:37 -0400

    docker-compose attempt1

---
+++ sql/init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE randomWords;
+
+INSERT INTO randomWords VALUES ('apk');
+INSERT INTO randomWords VALUES ('cake');
+INSERT INTO randomWords VALUES ('car');
+INSERT INTO randomWords VALUES ('tar');
